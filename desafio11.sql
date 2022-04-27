bc408491c47a14dacbd522759ff510240d5fcef8
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-04-27 19:54:04 -0300

    adicionado resolução alternativa ao requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,9 @@
 SELECT notes FROM northwind.purchase_orders WHERE notes REGEXP '3[0-9]';
+
+-- OR
+/*
+SELECT notes
+FROM northwind.purchase_orders
+WHERE notes
+BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
+*/
